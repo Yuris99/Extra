bbb9a6bb695e97a64acd5761fb3384a3bb81a3bd
Yuris99 <mose1207@gmail.com>
2021-12-02 17:15:46 +0900

    update

--- 과제/Algorithm/Assignment3/20201777.cpp
+++ 과제/Algorithm/Assignment3/20201777.cpp
@@ -9,6 +9,38 @@ int W[20];
 int V[20];
 
 int maxprofit = -1;
+int numbest = 0;
+
+class Queue {
+    private:
+        int* data;
+    public:
+        int head;
+        int tail;
+        int size;
+        int length;
+        
+
+    public:
+        Queue() {
+            head = 0;
+            tail = 0;
+            data = new int(20);
+            size = 20;
+            length = 0;
+        }
+
+        bool empty() { return head == tail; }
+        void push() {
+            tail++;
+            length++;
+            if(tail >= size) {
+                
+            }
+        }
+
+    
+};
 
 int Input() {
     cin >> N >> K;
@@ -18,15 +50,19 @@ int Input() {
 }
 
 void BTknapsack(int i, int profit, int weight) {
-    if(weight <= K && profit > maxprofit) {
-
+    if(weight <= K && profit > maxprofit) 
+        maxprofit = profit;
+    if(promising(i, profit, weight)) {
+        BTknapsack(i+1, profit+V[i], weight+W[i]);
+        BTknapsack(i+1, profit, weight);
     }
+
 }
 
 bool promising(int i, int profit, int weight) {
-    int j, k;
+    int j;
     int totweight;
-    float bound;
+    int bound;
     if(weight >= K) return false;
     else {
         j = i + 1;
@@ -37,15 +73,23 @@ bool promising(int i, int profit, int weight) {
             bound += V[j];
             j++;
         }
-        k = j;
-        if(k <= N)
-            bound += (K-totweight)*V[k]/W[k];
         return bound > maxprofit;
     }
 }
 
 void Backtracking() {
-    
+    BTknapsack(0, 0, 0);
+}
+
+void Brknapsack2(int n, const int* p, const int* w, int &maxprofit) {
+    Queue Q;
+    while(!Q.empty()) {
+
+    }
+}
+
+int bound(int u) {
+
 }
 
 void Breadth() {
